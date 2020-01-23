e6cab5f5e83e8e5380c28b105044d4e157e02183
Mikel Wassenius <mikel.wassenius@vinnter.se>
2020-01-23 12:48:36 +0100

    added simple intro to the about page

--- poetry-slack-client/src/components/About/howto.jsx
+++ poetry-slack-client/src/components/About/howto.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const HowTo = () => (
    <div className="column">
-      <h2 className="title">How to use this website</h2>
+      <h2 className="title">About this app</h2>
+      <p className='subtitle'>Welcome to the Poetry Slack app!</p>
+      <p className='subtitle'>- The app that lets you generate or make poetry from your slack-history.</p>
+      <p className='subtitle'>- To get started simply pick your poetry genre of choice and follow the instructions.</p>
    </div>
 )
 
