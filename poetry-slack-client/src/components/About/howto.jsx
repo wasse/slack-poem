0955369544bca14fcc9d5a2fa465580f7ecabfb0
Mikel Wassenius <mikel.wassenius@vinnter.se>
2020-01-23 09:03:50 +0100

    added footer component, started modifying about-tab

---
+++ poetry-slack-client/src/components/About/howto.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HowTo = () => (
+   <div className="column">
+      <h2 className="title">How to use this website</h2>
+   </div>
+)
+
+export default HowTo
--- poetry-slack-client/src/pages/BasePage.jsx
+++ poetry-slack-client/src/pages/BasePage.jsx
@@ -5,6 +5,7 @@ import Header from '../components/Header'
 import Sidemenu from '../components/Sidemenu/Sidemenu'
 import BasePageRoutes from '../routes/BasePageRoutes'
 import About from '../components/About'
+import Footer from '../components/Footer'
 import { observer } from 'mobx-react'
 import { useStores } from '../custom-hooks/use-stores'
 import clsx from 'clsx'
@@ -22,6 +23,7 @@ const BasePage = observer(() => {
                <BasePageRoutes />
             </div>
          </div>
+         <Footer />
       </div>
    )
 })
