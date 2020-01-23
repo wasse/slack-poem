b1de8d4101c6d5adb429513965679b873beb631f
MelindaSW <Sandstrommelinda@gmail.com>
2020-01-23 12:01:17 +0100

    Add spinner in haiku feature

---
+++ poetry-slack-client/src/components/Spinners/Spinner.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { PulseSpinner } from 'react-spinners-kit'
+
+const Spinner = () => {
+   return <PulseSpinner size={80} color="#b63e29" />
+}
+
+export default Spinner
